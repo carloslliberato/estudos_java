a083db86a313be48d7b61568f1016d0b3a181368
carloslliberato <carloslsliberato@gmail.com>
2026-06-27 17:58:28 -0300

    basicos

---
+++ revisao/basicos/Addition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int number1, number2, sum;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter first integer: ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("Sum is %d%n", sum);
+        System.out.printf("Sum is %d%n", (number1 + number2));
+    }
+}
